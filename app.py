6cb20c40c599bc0ab474b695e5d70f00e1c263d7
liramsalem <liramsalem@gmail.com>
2021-12-28 20:16:40 +0200

    ex 9

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from flask import request
+from flask import session
 
 app = Flask(__name__)
+app.secret_key = '123'
 
 @app.route('/cv')
 @app.route('/')
@@ -24,6 +27,41 @@ def assignment8_fun():
                             Hobbies=['art','music','sql','flask', 'animal', 'web']
                            )
 
+
+@app.route('/assignment9', methods=['GET','POST'])
+def assignment9_func():
+    if request.method == 'GET':
+        users = {'user1': {'name': 'Yosi', 'email': 'yo@gmail.com'},
+                 'user2': {'name': 'Lir', 'email': 'lir@gmail.com'},
+                 'user3': {'name': 'Tal', 'email': 'tal@gmail.com'},
+                 'user4': {'name': 'Yarin', 'email': 'yarin@gmail.com'},
+                 'user5': {'name': 'Yaron', 'email': 'yaron@gmail.com'},
+                 'user6': {'name': 'Eli', 'email': 'eli@gmail.com'}
+                 }
+
+        if 'name' in request.args:
+            name = request.args['name']
+            if name == '':
+                name = request.args['name']
+                return render_template('assignment9.html', dic_users=users)
+            else:
+                for key in users:
+                    for key2 in users[key]:
+                        if users[key][key2]== name:
+                            return render_template('assignment9.html', c_name=users[key]['name'], c_email=users[key]['email'])
+                return render_template('assignment9.html', message='error')
+
+
+    if request.method == 'POST':
+        if 'logOutButton' in request.form:
+            session['username'] = ''
+        if 'username' in request.form:
+            Username = request.form['username']
+            Email= request.form['email']
+            Password = request.form['password']
+            session['username'] = Username
+
+    return render_template('assignment9.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
